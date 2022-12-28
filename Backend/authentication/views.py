132517355cc562eb8fd4d8c7732be88a48cff43f
ronish2722 <ronishshrestha2722@gmail.com>
2022-12-28 09:43:11 +0545

    Back end progress

---
+++ Backend/authentication/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, "authentication/index.html")
+
+
+def signup(request):
+    return render(request, "authentication/signup.html")
+
+
+def signin(request):
+    return render(request, "authesntication/signin.html")
+
+
+def signout(request):
+    pass
+# Create your views here.
---
+++ Frontend/ghar-sewa/src/components/LoginForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+import images from "../images/fyp2.jpeg";
+
+const LoginForm = () => {
+  return (
+    <div>{/* <img src={images} alt="home service" className="" /> */}</div>
+  );
+};
+export default LoginForm;
